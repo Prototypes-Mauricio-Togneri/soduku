ba985557c692e322cb39972781c8e04e45cc609c
Mauricio Togneri <mauricio.togneri@gmail.com>
2025-03-13 19:25:25 +0100

    Improvements

--- lib/model/grid.dart
+++ lib/model/grid.dart
@@ -1,1 +1,27 @@
-class Grid {}
+import 'dart:io';
+
+typedef Row = List<int>;
+
+class Grid {
+  final List<Row> rows;
+
+  const Grid({required this.rows});
+
+  factory Grid.fromString(String input) {
+    final List<String> lines = input.split('\n');
+    final List<Row> rows = [];
+
+    for (final String line in lines) {
+      final List<int> row = line.split(' ').map(int.parse).toList();
+      rows.add(row);
+    }
+
+    return Grid(rows: rows);
+  }
+
+  factory Grid.fromFile(String filePath) {
+    final String input = File(filePath).readAsStringSync();
+
+    return Grid.fromString(input);
+  }
+}
--- test/example_test.dart
+++ test/example_test.dart
@@ -1,9 +1,11 @@
+import 'package:sudoku_solver/model/grid.dart';
 import 'package:test/test.dart';
 
 void main() {
   group('Group example', () {
     test('Test 1', () {
-      expect(123, 123);
+      final Grid grid = Grid.fromFile('./test/resources/example1.txt');
+      expect(grid.rows.length, 9);
     });
   });
 }
