c62017690662de664336fc73e3032768aa90ab55
Mauricio Togneri <mauricio.togneri@gmail.com>
2025-03-13 22:13:54 +0100

    Improvements

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,4 @@
 import 'dart:io';
-import 'dart:math';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:google_mlkit_text_recognition/google_mlkit_text_recognition.dart';
@@ -43,19 +42,8 @@ class MyHomePage extends StatelessWidget {
   }
 
   Future _parseImage() async {
-    final String data = await rootBundle.loadString(
-      'assets/example/sudoku.png',
-    );
-
-    final Directory directory = await getApplicationDocumentsDirectory();
-    final String tempPath = directory.path;
-    final File file = File('$tempPath/sudoku.png');
-    await file.writeAsBytes(Uint8List.fromList(data.codeUnits));
-
-    final InputImage inputImage = InputImage.fromFile(file);
-    final TextRecognizer textRecognizer = TextRecognizer(
-      script: TextRecognitionScript.latin,
-    );
+    final InputImage inputImage = await _inputImage();
+    final TextRecognizer textRecognizer = TextRecognizer();
     final RecognizedText recognizedText = await textRecognizer.processImage(
       inputImage,
     );
@@ -64,17 +52,8 @@ class MyHomePage extends StatelessWidget {
     print(text);
 
     for (final TextBlock block in recognizedText.blocks) {
-      final Rect rect = block.boundingBox;
-      print(rect);
-
-      final List<Point<int>> cornerPoints = block.cornerPoints;
-      print(cornerPoints);
-
       final String text = block.text;
       print(text);
-
-      final List<String> languages = block.recognizedLanguages;
-      print(languages);
 
       for (final TextLine line in block.lines) {
         for (final TextElement element in line.elements) {
@@ -83,5 +62,16 @@ class MyHomePage extends StatelessWidget {
         }
       }
     }
+  }
+
+  Future<InputImage> _inputImage() async {
+    final ByteData buffer = await rootBundle.load('assets/example/sudoku.png');
+
+    final Directory directory = await getApplicationDocumentsDirectory();
+    final String tempPath = directory.path;
+    final File file = File('$tempPath/sudoku.png');
+    await file.writeAsBytes(buffer.buffer.asUint8List());
+
+    return InputImage.fromFile(file);
   }
 }
