e6986c5db1255890c035ea5a8b635ce087ab1f9a
Mauricio Togneri <mauricio.togneri@gmail.com>
2025-03-15 20:01:34 +0100

    Improvements

--- lib/home.dart
+++ lib/home.dart
@@ -1,5 +1,4 @@
 import 'dart:io';
-
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:google_mlkit_document_scanner/google_mlkit_document_scanner.dart';
